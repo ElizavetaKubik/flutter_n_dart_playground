39103d54951e5fa259452b53307f158e0ada249b
Elizaveta <65027713+ElizavetaKubik@users.noreply.github.com>
2023-03-07 23:58:20 +0300

    telegram_ui init

---
+++ telegram_ui/lib/main.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:telegram_ui/pages/root_app.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RootApp(),
+    ),
+  );
+}
---
+++ telegram_ui/lib/pages/root_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:telegram_ui/theme/colors.dart';
+
+class RootApp extends StatefulWidget {
+  const RootApp({super.key});
+
+  @override
+  State<RootApp> createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+    );
+  }
+}
